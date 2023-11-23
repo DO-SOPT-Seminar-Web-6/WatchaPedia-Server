4259afc99ed00b4c636a538410d560ce5d3f3289
hysong4u <hayeon_song@naver.com>
2023-11-24 03:34:38 +0900

    [feat] #2 출연/제작 엔티티 구현

---
+++ src/main/java/org/sopt/watchapedia/domain/production/domain/Production.java
@@ -0,0 +1,33 @@
+package org.sopt.watchapedia.domain.production.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Entity
+public class Production {
+
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    @Column(name = "production_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String imageUrl;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String role;
+
+    @Column(nullable = true)
+    private String roleDetail;
+
+    @Column(columnDefinition = "TEXT", nullable = true)
+    private String biography;
+
+
+}
