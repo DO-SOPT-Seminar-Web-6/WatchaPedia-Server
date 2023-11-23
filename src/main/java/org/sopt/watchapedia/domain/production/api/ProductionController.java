7605f18e906610f1358d0abd87e64a48d6ead436
hysong4u <hayeon_song@naver.com>
2023-11-24 04:18:24 +0900

    [feat]출연/제작 상세 조회 API 구현

--- src/main/java/org/sopt/watchapedia/domain/production/api/ProductionController.java
+++ src/main/java/org/sopt/watchapedia/domain/production/api/ProductionController.java
@@ -1,14 +1,13 @@
 package org.sopt.watchapedia.domain.production.api;
 
 import lombok.RequiredArgsConstructor;
+import org.sopt.watchapedia.domain.production.dto.response.ProductionDetailResponse;
 import org.sopt.watchapedia.domain.production.dto.response.ProductionGetResponse;
 import org.sopt.watchapedia.domain.production.service.ProductionService;
 import org.sopt.watchapedia.global.common.ApiResponse;
 import org.sopt.watchapedia.global.common.SuccessStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,4 +24,11 @@ public class ProductionController {
         List<ProductionGetResponse> productions = productionService.getProductions();
         return ApiResponse.success(SuccessStatus.OK, productions);
     }
+
+    @GetMapping("/production/{productionId}")
+    public ResponseEntity<ApiResponse<?>> getProductionDetail(@PathVariable final Long productionId) {
+        ProductionDetailResponse productionDetail = productionService.getProductionDetail(productionId);
+        return ApiResponse.success(SuccessStatus.OK, productionDetail);
+    }
+
 }
