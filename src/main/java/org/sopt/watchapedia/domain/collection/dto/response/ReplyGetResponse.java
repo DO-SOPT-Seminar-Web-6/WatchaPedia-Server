def6f99d6e4ca9e3ca921c6e3df6b00888d10d9f
sunwoong <sunwoong1122@gmail.com>
2023-11-27 12:40:36 +0900

    [fix] #10 컬렉션 상세 조회 API 응답 스펙 변경

--- src/main/java/org/sopt/watchapedia/domain/collection/dto/response/ReplyGetResponse.java
+++ src/main/java/org/sopt/watchapedia/domain/collection/dto/response/ReplyGetResponse.java
@@ -6,11 +6,13 @@ import org.sopt.watchapedia.domain.collection.domain.Reply;
 
 @Builder(access = AccessLevel.PRIVATE)
 public record ReplyGetResponse(
+        Long replyId,
         String content,
         int likeCount
 ) {
     public static ReplyGetResponse of(Reply reply) {
         return ReplyGetResponse.builder()
+                .replyId(reply.getId())
                 .content(reply.getContent())
                 .likeCount(reply.getLikeCount())
                 .build();
